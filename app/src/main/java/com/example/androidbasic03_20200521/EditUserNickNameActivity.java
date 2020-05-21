50e7bd40bf2dc9fc51e1ed014b6a0bbe2a10962e
weekyeon <weekyeon@gmail.com>
2020-05-21 16:01:16 +0900

    닉네임 변경 프로세스  - 다른 액티비티를 통해 입력값 받아서 반영

--- app/src/main/java/com/example/androidbasic03_20200521/EditUserNickNameActivity.java
+++ app/src/main/java/com/example/androidbasic03_20200521/EditUserNickNameActivity.java
@@ -1,15 +1,23 @@
 package com.example.androidbasic03_20200521;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic03_20200521.databinding.ActivityEditUserNickNameBinding;
 
 public class EditUserNickNameActivity extends BaseActivity {
+
+    ActivityEditUserNickNameBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_nick_name);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_user_nick_name);
         setUpEvents();
         setValues();
     }
@@ -17,10 +25,26 @@ public class EditUserNickNameActivity extends BaseActivity {
     @Override
     public void setUpEvents() {
 
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nickName = binding.nickNameEdt.getText().toString();
+
+                Intent myIntent = new Intent();
+                myIntent.putExtra("editNickName", nickName);
+
+                setResult(RESULT_OK, myIntent);
+
+                finish();
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
 
     }
+
+
 }
--- app/src/main/java/com/example/androidbasic03_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic03_20200521/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.androidbasic03_20200521;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.androidbasic03_20200521.databinding.ActivityMainBinding;
 
@@ -22,10 +25,36 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setUpEvents() {
 
+        View.OnClickListener ocl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext, EditUserNickNameActivity.class);
+                startActivityForResult(myIntent, 1004);
+            }
+        };
+
+        binding.editNickNameBtn.setOnClickListener(ocl);
+
     }
 
     @Override
     public void setValues() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1004) {
+            if (resultCode == RESULT_OK) {
+
+                String nick = data.getStringExtra("editNickName");
+
+                binding.userNickNmaeTxt.setText(nick);
+
+            }
+        }
 
     }
 }
