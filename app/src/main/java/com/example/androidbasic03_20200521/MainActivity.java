62d3be9471308ca600201826aa6080da3a0638fb
weekyeon <weekyeon@gmail.com>
2020-05-21 17:49:49 +0900

    입력된 전화번호에 연결하도록 수정

--- app/src/main/java/com/example/androidbasic03_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic03_20200521/MainActivity.java
@@ -37,7 +37,9 @@ public class MainActivity extends BaseActivity {
 
 //                전화걸기 등 안드로이드 => Uri 정보 요구
 
-                Uri myUri = Uri.parse("tel:010-8077-3619");
+                String dialPhoneNum = binding.userPhoneTxt.getText().toString();
+
+                Uri myUri = Uri.parse(String.format("tel:%s", dialPhoneNum));
                 Intent myIntent = new Intent(Intent.ACTION_DIAL, myUri);
                 startActivity(myIntent);
 
