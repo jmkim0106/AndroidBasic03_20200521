468eb175b092b1a7f98b8f6691a47e04e29d58f1
weekyeon <weekyeon@gmail.com>
2020-05-21 15:37:00 +0900

    데이터바인딩 연결

--- app/src/main/java/com/example/androidbasic03_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic03_20200521/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.androidbasic03_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.androidbasic03_20200521.databinding.ActivityMainBinding;
+
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setUpEvents();
         setValues();
     }
