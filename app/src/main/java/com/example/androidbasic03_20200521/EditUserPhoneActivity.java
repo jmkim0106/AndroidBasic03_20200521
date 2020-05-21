2c72d90650fd119bceb9aaa33d59a6257cccca82
weekyeon <weekyeon@gmail.com>
2020-05-21 16:32:44 +0900

    변경하기 버튼 누르면 => 입력한 폰번 추출까지

--- app/src/main/java/com/example/androidbasic03_20200521/EditUserPhoneActivity.java
+++ app/src/main/java/com/example/androidbasic03_20200521/EditUserPhoneActivity.java
@@ -1,21 +1,36 @@
 package com.example.androidbasic03_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic03_20200521.databinding.ActivityEditUserPhoneBinding;
 
 public class EditUserPhoneActivity extends BaseActivity {
+
+    ActivityEditUserPhoneBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_phone);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_user_phone);
         setUpEvents();
         setValues();
     }
 
     @Override
     public void setUpEvents() {
+
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputPhoneNum = binding.phoneNumEdit.getText().toString();
+
+            }
+        });
 
     }
 
